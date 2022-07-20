4e20846df367c9270cc6ce8e57ef2a6a6afb3111
Moisés Lemos <93021767+moiseslemosz@users.noreply.github.com>
2022-07-20 16:04:20 -0300

    Update js.js

--- Codigo final/js/js.js
+++ Codigo final/js/js.js
@@ -350,22 +350,22 @@ function start() {
 			
 			if (energiaAtual==3) {
 				
-				$("#energia").css("background-image", "url(imgs/energia3.png)");
+				$("#energia").css("background-image", "url:(imgs/energia3.png)");
 			}
 
 			if (energiaAtual==2) {
 				
-				$("#energia").css("background-image", "url(imgs/energia2.png)");
+				$("#energia").css("background-image", "url:(imgs/energia2.png)");
 			}
 
 			if (energiaAtual==1) {
 				
-				$("#energia").css("background-image", "url(imgs/energia1.png)");
+				$("#energia").css("background-image", "url:(imgs/energia1.png)");
 			}
 
 			if (energiaAtual==0) {
 				
-				$("#energia").css("background-image", "url(imgs/energia0.png)");
+				$("#energia").css("background-image", "url:(imgs/energia0.png)");
 				
 				gameOver();
 			}
