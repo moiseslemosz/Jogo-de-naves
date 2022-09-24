7aa52f03665de3c2ce23761139197e819d624d03
Moisés Lemos <93021767+moiseslemosz@users.noreply.github.com>
2022-09-24 20:04:14 -0300

    Update js.js

--- Codigo final/js/js.js
+++ Codigo final/js/js.js
@@ -387,7 +387,7 @@ function start() {
 			
 			$("#fundoGame").append("<div id='fim'></div>");
 			
-			$("#fim").html("<h1> Game Over </h1><p class="mm">Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+			$("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
 		} 
 } 
 		
